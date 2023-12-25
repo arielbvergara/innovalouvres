9bba9cb72be5ed464c7fcb67b36887e336f0cf62
Ariel <arielbernardovergara@gmail.com>
2023-12-25 17:52:21 +0100

    arranging header links

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Header({}) {
     return (
-        <div className="fixed left-0 top-0 h-20 size-full flex justify-items-center justify-evenly align-middle bg-white">
+        <div className="fixed left-0 top-0 h-20 size-full flex justify-items-center justify-evenly align-middle bg-white max-sm:justify-">
            <div className="logo flex-none m-auto">
             <Link href="/">
                 <Image
@@ -16,18 +16,21 @@ export default function Header({}) {
             </Link>
            </div>
            <div className="pages flex justify-items-center justify-evenly  m-auto">
-                <Link href="/" className="m-2">
+                <Link href="/" className="m-2 lg:m-6 md:m-4 max-sm:hidden">
                     Home
                 </Link>
-                <Link href="/gallery" className="m-2">
+                <Link href="/gallery" className="m-2 lg:m-6 md:m-4 max-sm:hidden">
                     Gallery
                 </Link>
-                <Link href="/about" className="m-2">
+                <Link href="/about" className="m-2 lg:m-6 md:m-4 max-sm:hidden">
                     About
                 </Link>
-                <Link href="/contact" className="m-2">
+                <Link href="/contact" className="m-2 lg:m-6 md:m-4 max-sm:hidden">
                     Contact
                 </Link>
+           </div>
+           <div className="burguer sm:hidden">
+                    HAMBURGER ICON
            </div>
         </div>
     )
