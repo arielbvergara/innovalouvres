11c769d00a8893e084b40f872b41c544f21e087b
ariel.vergara <ariel.vergara@dustin.com>
2023-12-29 09:15:40 +0100

    adding color to the elements in the header

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,17 @@
-    import { faBars } from "@fortawesome/free-solid-svg-icons";
+'use client'
+
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from 'next/navigation'
 
 export default function Header({}) {
+
+    const path = usePathname()
+
+    console.log(path)
+    
     return (
         <div className="left-0 top-0 z-50 flex size-full h-[--header-height] justify-between bg-white text-grey">
            <div className="logo mx-4 my-auto flex-none sm:mx-auto">
@@ -18,16 +26,16 @@ export default function Header({}) {
             </Link>
            </div>
            <div className="pages m-auto flex hidden sm:block">
-                <Link href="/" className="m-2 hover:text-light-orange md:m-4 lg:m-6">
+                <Link href="/" className={path === "/" ? "m-2 hover:text-light-orange md:m-4 lg:m-6 active" : "m-2 hover:text-light-orange md:m-4 lg:m-6"} >
                     Home
                 </Link>
-                <Link href="/gallery" className="m-2 hover:text-light-orange md:m-4 lg:m-6">
+                <Link href="/gallery" className={path === "/gallery" ? "m-2 hover:text-light-orange md:m-4 lg:m-6 active" : "m-2 hover:text-light-orange md:m-4 lg:m-6"}>
                     Gallery
                 </Link>
-                <Link href="/about" className="m-2 hover:text-light-orange md:m-4 lg:m-6">
+                <Link href="/about" className={path === "/about" ? "m-2 hover:text-light-orange md:m-4 lg:m-6 active" : "m-2 hover:text-light-orange md:m-4 lg:m-6"}>
                     About
                 </Link>
-                <Link href="/contact" className="m-2 hover:text-light-orange md:m-4 lg:m-6">
+                <Link href="/contact" className={path === "/contact" ? "m-2 hover:text-light-orange md:m-4 lg:m-6 active" : "m-2 hover:text-light-orange md:m-4 lg:m-6"}>
                     Contact
                 </Link>
            </div>
