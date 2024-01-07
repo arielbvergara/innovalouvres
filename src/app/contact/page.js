c40c8af4263567fe9a365511414d8dd9d8cc09cb
ariel.vergara <ariel.vergara@dustin.com>
2024-01-07 18:01:27 +0100

    Adding more fields to contact us page

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -28,24 +28,34 @@ export default function Contact({}) {
             </section>
 
             <section className="mx-4 mb-8">
-                <h2 className="mb-4 text-2xl text-orange">Contact Form</h2>
+                <h2 className="mb-4 text-2xl text-orange">Contact Us</h2>
                 <form action="#" method="post" className="grid grid-cols-1 gap-4 md:grid-cols-2">
-                    <div className="col-span-2 mb-4 sm:col-span-1">
-                        <label for="name" className="block text-sm font-medium text-gray-700">Your Name</label>
+                    <div className="col-span-3 mb-4 sm:col-span-1">
+                        <label for="name" className="block text-sm font-medium text-gray-700">Name</label>
                         <input type="text" id="name" name="name" required
                             className="mt-1 w-full rounded-md border border-gray-300 p-2 focus:border-blue-500 focus:outline-none" />
                     </div>
-                    <div className="col-span-2 mb-4 sm:col-span-1">
-                        <label for="email" className="block text-sm font-medium text-gray-700">Your Email</label>
+                    <div className="col-span-3 mb-4 sm:col-span-1">
+                        <label for="email" className="block text-sm font-medium text-gray-700">Email*</label>
                         <input type="email" id="email" name="email" required
                             className="mt-1 w-full rounded-md border border-gray-300 p-2 focus:border-blue-500 focus:outline-none" />
                     </div>
-                    <div className="col-span-2 mb-4">
-                        <label for="message" className="block text-sm font-medium text-gray-700">Your Message</label>
-                        <textarea id="message" name="message" rows="4" required
+                    <div className="col-span-3 mb-4 sm:col-span-1">
+                        <label for="phone" className="block text-sm font-medium text-gray-700">Phone number</label>
+                        <input type="phone" id="phone" name="phone" required
+                            className="mt-1 w-full rounded-md border border-gray-300 p-2 focus:border-blue-500 focus:outline-none" />
+                    </div>
+                    <div className="col-span-3 mb-4">
+                        <label for="message-after" className="block text-sm font-medium text-gray-700">Let us know what you are after</label>
+                        <textarea id="message-after" name="message-after" rows="4" required
                             className="mt-1 w-full rounded-md border border-gray-300 p-2 focus:border-blue-500 focus:outline-none"></textarea>
                     </div>
-                    <div className="col-span-2">
+                    <div className="col-span-3 mb-4">
+                        <label for="message-size" className="block text-sm font-medium text-gray-700">Do you know the sizes of your dreamed louvre? Let us know here!</label>
+                        <textarea id="message-size" name="message-size" rows="4" required
+                            className="mt-1 w-full rounded-md border border-gray-300 p-2 focus:border-blue-500 focus:outline-none"></textarea>
+                    </div>
+                    <div className="col-span-3">
                         <button type="submit"
                             className="w-full rounded-md bg-orange p-2 text-white hover:bg-blue-600 focus:bg-blue-600 focus:outline-none">
                             Send Message
