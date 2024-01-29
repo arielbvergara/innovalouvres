aa96791cfdce3bbf99ca9d5739091572255dd0d4
Ariel <arielbernardovergara@gmail.com>
2024-01-29 09:01:33 +0100

    onboarding resend

--- src/app/api/sendEmail/route.js
+++ src/app/api/sendEmail/route.js
@@ -5,18 +5,18 @@ import { Resend } from 'resend';
 
 export async function POST(request) {
     const body = await request.json();
-    const resend = new Resend(process.env.NEXT_PUBLIC_RESEND_API_KEY);
+    // const resend = new Resend(process.env.NEXT_PUBLIC_RESEND_API_KEY);
     try{
       console.log("body", body)
 
-      let response = resend.emails.send({
-        from: body.emailTo,
-        to: body.emailTo,
-        subject: body.subject,
-        html: body.otpText
-      });
+      const resend = new Resend('re_CPqynsVB_ALKAmu7A77d1M6mR7q4ravbE');
 
-      console.log("response", response)
+      resend.emails.send({
+        from: 'onboarding@resend.dev',
+        to: 'arielbernardovergara@gmail.com',
+        subject: 'Hello World',
+        html: '<p>Congrats on sending your <strong>first email</strong>!</p>'
+      });
 
       return NextResponse.json({message: "EMAIL_SENT_SUCCESSFULLY"}, {status: 200})
     } catch (error) {
