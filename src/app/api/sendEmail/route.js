2e4461760c3acae4a0f89c6dfcf3859ecd4265b2
Ariel <arielbernardovergara@gmail.com>
2024-01-29 08:57:19 +0100

    adding response console.log

--- src/app/api/sendEmail/route.js
+++ src/app/api/sendEmail/route.js
@@ -7,12 +7,16 @@ export async function POST(request) {
     const body = await request.json();
     const resend = new Resend(process.env.NEXT_PUBLIC_RESEND_API_KEY);
     try{
-      resend.emails.send({
+      console.log("body", body)
+
+      let response = resend.emails.send({
         from: body.emailTo,
         to: body.emailTo,
         subject: body.subject,
         html: body.otpText
       });
+
+      console.log("response", response)
 
       return NextResponse.json({message: "EMAIL_SENT_SUCCESSFULLY"}, {status: 200})
     } catch (error) {
