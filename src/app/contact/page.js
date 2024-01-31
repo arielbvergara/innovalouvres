1091efb3f3bf66e449c44ff40962b6c5666afe76
Ariel <arielbernardovergara@gmail.com>
2024-01-31 18:09:32 +0100

    Adding thank you page

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -4,6 +4,7 @@ import { useContext, useEffect, useState } from "react";
 import { AlertSuccess, AlertWarning } from "../../components/alert";
 import Loading from "@/components/loading";
 import { ContentContext } from "@/components/app";
+import ThankYou from "../thankyou/page";
 
 const formatEmail = (name, email, phone, messageAfter, messageSize) => {
     return `
@@ -26,7 +27,7 @@ export default function Contact() {
     const [messageAfter, setMessageAfter] = useState('');
     const [messageSize, setMessageSize] = useState('');
     const [emailSuccess, setEmailSuccess] = useState(null)
-    
+
     const handleSubmit = async (e) => {
         e.preventDefault();
         
@@ -44,7 +45,7 @@ export default function Contact() {
         });
 
         const success = await response.json();
-        setEmailSuccess(success)
+        setEmailSuccess(success)        
     }
 
     const contentContext = useContext(ContentContext)
@@ -60,59 +61,63 @@ export default function Contact() {
 
     return (
         contactContent ? 
-            (<div className="container mx-auto min-h-[--body-min-height]">
-                <header className="py-8 text-center">
-                    <h1 className="text-4xl">{contactContent.title}</h1>
-                </header>
-                <section className="mx-4 mb-8">
-                    <p className="text-lg">
-                        {contactContent.subTitle}
-                    </p>
-                </section>
-                <section className="mx-4 mb-8">
-                    <form method="post" className="grid grid-cols-1 gap-4 md:grid-cols-2" onSubmit={handleSubmit}>
-                        <div className="col-span-3 mb-4 sm:col-span-1">
-                            <label htmlFor="name" className="block text-sm font-medium text-gray-700">{contactContent.form.name}</label>
-                            <input type="text" id="name" name="name" required value={name} onChange={(e) => {setName(e.target.value)}}
-                                className="mt-1 w-full rounded-md border border-gray-300 p-2 focus:border-orange focus:outline-none" />
-                        </div>
-                        <div className="col-span-3 mb-4 sm:col-span-1">
-                            <label htmlFor="email" className="block text-sm font-medium text-gray-700">{contactContent.form.email}</label>
-                            <input type="email" id="email" name="email" required value={email} onChange={(e) => {setEmail(e.target.value)}}
-                                className="mt-1 w-full rounded-md border border-gray-300 p-2 focus:border-orange focus:outline-none" />
-                        </div>
-                        <div className="col-span-3 mb-4 sm:col-span-1">
-                            <label htmlFor="phone" className="block text-sm font-medium text-gray-700">{contactContent.form.phone}</label>
-                            <input type="phone" id="phone" name="phone" required value={phone} onChange={(e) => {setPhone(e.target.value)}}
-                                className="mt-1 w-full rounded-md border border-gray-300 p-2 focus:border-orange focus:outline-none" />
-                        </div>
-                        <div className="col-span-3 mb-4">
-                            <label htmlFor="message-after" className="block text-sm font-medium text-gray-700">{contactContent.form.messageOne}</label>
-                            <textarea id="message-after" name="message-after" rows="4" required value={messageAfter} onChange={(e) => {setMessageAfter(e.target.value)}}
-                                className="mt-1 w-full rounded-md border border-gray-300 p-2 focus:border-orange focus:outline-none"></textarea>
-                        </div>
-                        <div className="col-span-3 mb-4">
-                            <label htmlFor="message-size" className="block text-sm font-medium text-gray-700">{contactContent.form.messageTwo}</label>
-                            <textarea id="message-size" name="message-size" rows="4" required value={messageSize} onChange={(e) => {setMessageSize(e.target.value)}}
-                                className="mt-1 w-full rounded-md border border-gray-300 p-2 focus:border-orange focus:outline-none"></textarea>
-                        </div>
-                        <div className="col-span-3">
-                            <button type="submit"
-                                className="send-message w-full rounded-md p-2 text-white hover:bg-orange focus:bg-orange focus:outline-none">
-                                {contactContent.form.send}
-                            </button>
-                        </div>
-                    </form>
-                </section>
-
-                {emailSuccess && (
-                    emailSuccess.message === "EMAIL_SENT_FAILED" ? 
-                        (<AlertWarning setEmailSuccess={setEmailSuccess}>Something went wrong, please try again later</AlertWarning>) : 
-                        (<AlertSuccess setEmailSuccess={setEmailSuccess}>Email sent successfully</AlertSuccess>)
-                    )
-                }
-            </div>)
+            (
+            
+                emailSuccess?.message === "EMAIL_SENT_SUCCESSFULLY" ? 
+                (
+                    <ThankYou />
+                )
+                :
+                (
+                    <div className="container mx-auto min-h-[--body-min-height]">
+                        <header className="py-8 text-center">
+                            <h1 className="text-4xl">{contactContent.title}</h1>
+                        </header>
+                        <section className="mx-4 mb-8">
+                            <p className="text-lg">
+                                {contactContent.subTitle}
+                            </p>
+                        </section>
+                        <section className="mx-4 mb-8">
+                            <form method="post" className="grid grid-cols-1 gap-4 md:grid-cols-2" onSubmit={handleSubmit}>
+                                <div className="col-span-3 mb-4 sm:col-span-1">
+                                    <label htmlFor="name" className="block text-sm font-medium text-gray-700">{contactContent.form.name}</label>
+                                    <input type="text" id="name" name="name" required value={name} onChange={(e) => {setName(e.target.value)}}
+                                        className="mt-1 w-full rounded-md border border-gray-300 p-2 focus:border-orange focus:outline-none" />
+                                </div>
+                                <div className="col-span-3 mb-4 sm:col-span-1">
+                                    <label htmlFor="email" className="block text-sm font-medium text-gray-700">{contactContent.form.email}</label>
+                                    <input type="email" id="email" name="email" required value={email} onChange={(e) => {setEmail(e.target.value)}}
+                                        className="mt-1 w-full rounded-md border border-gray-300 p-2 focus:border-orange focus:outline-none" />
+                                </div>
+                                <div className="col-span-3 mb-4 sm:col-span-1">
+                                    <label htmlFor="phone" className="block text-sm font-medium text-gray-700">{contactContent.form.phone}</label>
+                                    <input type="phone" id="phone" name="phone" required value={phone} onChange={(e) => {setPhone(e.target.value)}}
+                                        className="mt-1 w-full rounded-md border border-gray-300 p-2 focus:border-orange focus:outline-none" />
+                                </div>
+                                <div className="col-span-3 mb-4">
+                                    <label htmlFor="message-after" className="block text-sm font-medium text-gray-700">{contactContent.form.messageOne}</label>
+                                    <textarea id="message-after" name="message-after" rows="4" required value={messageAfter} onChange={(e) => {setMessageAfter(e.target.value)}}
+                                        className="mt-1 w-full rounded-md border border-gray-300 p-2 focus:border-orange focus:outline-none"></textarea>
+                                </div>
+                                <div className="col-span-3 mb-4">
+                                    <label htmlFor="message-size" className="block text-sm font-medium text-gray-700">{contactContent.form.messageTwo}</label>
+                                    <textarea id="message-size" name="message-size" rows="4" required value={messageSize} onChange={(e) => {setMessageSize(e.target.value)}}
+                                        className="mt-1 w-full rounded-md border border-gray-300 p-2 focus:border-orange focus:outline-none"></textarea>
+                                </div>
+                                <div className="col-span-3">
+                                    <button type="submit"
+                                        className="send-message w-full rounded-md p-2 text-white hover:bg-orange focus:bg-orange focus:outline-none">
+                                        {contactContent.form.send}
+                                    </button>
+                                </div>
+                            </form>
+                        </section>
+                    </div>
+                )    
+                
+            )
             :
-            (<Loading />)
+            <Loading />
     )
 }
---
+++ src/app/thankyou/page.js
@@ -0,0 +1,13 @@
+'use client'
+
+export default function ThankYou({}) {
+
+    return (
+        <div className="container mx-auto min-h-[--body-min-height] m-5">
+            <section className="mx-4 mb-8 py-8 text-center text-gray-500">
+            <h2 className="mb-4 text-2xl">Email sent successfully!</h2>
+                Thank you for contacting us! Your message has been successfully sent. We will get back to you as soon as possible.
+            </section>
+        </div>
+    )
+}
