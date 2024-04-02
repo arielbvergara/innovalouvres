1b92e87392a2c07a9da8858bf012379772a0f437
ariel.vergara <ariel.vergara@dustin.com>
2024-04-02 19:24:17 +0200

    Fixing carousel

--- src/components/galleryCarousel.js
+++ src/components/galleryCarousel.js
@@ -1,37 +1,41 @@
-import { Carousel } from "@material-tailwind/react";
+import { Carousel, Dialog, DialogBody } from "@material-tailwind/react";
 import Image from "next/image";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
-export function GalleryCarousel({ galleryImages, index }) {
+export function GalleryCarousel({ galleryImages, index, open, handleOpen }) {
 
     const { innerWidth: width, innerHeight: height } = window;
 
     useEffect(
         () => {
-            const nextButton = document.getElementsByClassName("gallery-carousel")[0].getElementsByTagName("span")[index];
-            nextButton.click()
+            const nextButton = document.getElementsByClassName("gallery-carousel")[0]?.getElementsByTagName("span")[index];
+            nextButton?.click()
         },
         [index])
 
     return (
-        <Carousel className="gallery-carousel" >
-            {
-                galleryImages.map(({ src, name, id }, index2) => (
-                    <div className="relative" key={index2}>
-                        <Image
-                            src={src}
-                            alt={name}
-                            className={(width < 900 || height < 500) ? "h-[75vh] w-full object-contain lg:h-[85vh]" : "w-full object-cover lg:h-[85vh]" }
-                            key={id}
-                            height={height}
-                            width={width}
-                            loading="lazy"
-                            placeholder="blur" 
-                            blurDataURL={src}
-                        />
-                    </div>
-                ))
-            }
-        </Carousel>
+        <Dialog size="xl" open={open} handler={handleOpen} className={(width < 900 || height < 500) ? "rounded-none bg-transparent":"rounded-none"}>
+            <DialogBody className="rounded-none p-0">
+                <Carousel className="gallery-carousel" >
+                    {
+                        galleryImages.map(({ src, name, id }, index2) => (
+                            <div className="relative" key={index2}>
+                                <Image
+                                    src={src}
+                                    alt={name}
+                                    className={(width < 900 || height < 500) ? "h-[75vh] w-full object-contain lg:h-[85vh]" : "w-full object-cover lg:h-[85vh]" }
+                                    key={id}
+                                    height={height}
+                                    width={width}
+                                    loading="lazy"
+                                    placeholder="blur" 
+                                    blurDataURL={src}
+                                />
+                            </div>
+                        ))
+                    }
+                </Carousel>
+            </DialogBody>
+        </Dialog>
     );
 }
