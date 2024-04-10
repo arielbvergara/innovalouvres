4665608718cb7d67ac80a4f6e12f85c857572ba6
ariel.vergara <ariel.vergara@dustin.com>
2024-04-10 17:24:33 +0200

    Removing slack implementation

--- src/app/api/sendEmail/route.js
+++ src/app/api/sendEmail/route.js
@@ -1,26 +1,34 @@
-import { WebClient } from '@slack/web-api';
+// import { WebClient } from '@slack/web-api';
 import { NextResponse } from 'next/server';
 import { Resend } from 'resend';
 
-const options = {};
-const web = new WebClient(process.env.NEXT_PUBLIC_SLACK_TOKEN, options);
+// const options = {};
+// const web = new WebClient(process.env.NEXT_PUBLIC_SLACK_TOKEN, options);
 
 export async function POST(request) {
     const body = await request.json();
     try{
-      const channel = process.env.NEXT_PUBLIC_SLACK_CHANNEL_ID
-      const resp = await web.conversations.join({
-          channel: channel,
+      // const channel = process.env.NEXT_PUBLIC_SLACK_CHANNEL_ID
+      // const resp = await web.conversations.join({
+      //     channel: channel,
+      // });
+      // if (body.otpText) {
+      //     await sendSlackMessage(body.otpText, channel);
+      // }
+      const resendAri = new Resend(process.env.NEXT_PUBLIC_RESEND_API_KEY_ARI);
+      await resendAri.emails.send({
+        from: `onboarding@resend.dev`,
+        to: 'arielbernardovergara@gmail.com',
+        subject: body.subject,
+        html: body.otpText
       });
-      if (body.otpText) {
-          await sendSlackMessage(body.otpText, channel);
-      }
     }
     catch(error) {
 
     }
-    const resend = new Resend(process.env.NEXT_PUBLIC_RESEND_API_KEY);
+    
     try{
+      const resend = new Resend(process.env.NEXT_PUBLIC_RESEND_API_KEY);
       await resend.emails.send({
         from: `Innovalouvres<${process.env.NEXT_PUBLIC_RESEND_API_USER}>`,
         to: process.env.NEXT_PUBLIC_EMAIL_TO,
@@ -33,19 +41,19 @@ export async function POST(request) {
       return NextResponse.json({message: "EMAIL_SENT_FAILED"}, {status: 500})
     }
 }
-const sendSlackMessage = async (message, channel = null) => {
+// const sendSlackMessage = async (message, channel = null) => {
   
-  return new Promise(async (resolve, reject) => {
-      const channelId = channel || process.env.NEXT_PUBLIC_SLACK_CHANNEL_ID;
-      try {
-          const resp = await web.chat.postMessage({
-            token: process.env.NEXT_PUBLIC_SLACK_TOKEN,
-            channel: channelId,
-            text: message,
-          });
-          return resolve(true);
-      } catch (error) {
-          return resolve(true);
-      }
-  });
-};
+//   return new Promise(async (resolve, reject) => {
+//       const channelId = channel || process.env.NEXT_PUBLIC_SLACK_CHANNEL_ID;
+//       try {
+//           const resp = await web.chat.postMessage({
+//             token: process.env.NEXT_PUBLIC_SLACK_TOKEN,
+//             channel: channelId,
+//             text: message,
+//           });
+//           return resolve(true);
+//       } catch (error) {
+//           return resolve(true);
+//       }
+//   });
+// };
